da80d6a09f8a154cc9e352fc26da0446b9b2d614
Travis Feldkamp <feldkatr@mail.uc.edu>
2023-02-16 17:50:44 -0500

    init proj

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int birthMonth = 65;// initialize varibale for user input of their birth month
+        if (birthMonth >=1 && birthMonth <=12) // if statement saying if the birth month is between 1 and 12 then execute the block of code
+        {
+            System.out.println("your birth month is: " + birthMonth);// output to user telling them their birth month
+        }
+        else {//else statement that says if initial conditions are not met run this block of code
+            System.out.println("You entered an incorrect month value: " + birthMonth);// output to user that tells them their input was invalid
+
+        }
+
     }
 }
